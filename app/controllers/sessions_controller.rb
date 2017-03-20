d288aa3bbcc36063ff444f782dcd723812b1a43e
Ashley Schauer <aelschauer@gmail.com>
2017-03-20 12:28:39 -0600

    add create and destroy functionality to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,18 @@
 class SessionsController < ApplicationController
   def create
+    oauth = RedditOauth.new(login_params)
+    token = oauth.token
+    data  = oauth.data
 
+    @user = User.from_reddit(data, token)
+    session[:user_id] = @user.id
+    redirect_to root_path
   end
 
   def destroy
+    session.clear
 
+    redirect_to root_path
   end
 
   private
